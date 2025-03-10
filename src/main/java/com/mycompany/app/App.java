686c2a1ddb261541cdbabdf63f2f94c3ba11050c
patia007 <163121886+patia007@users.noreply.github.com>
2025-03-10 19:34:01 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Helloo World!";
+    private static final String MESSAGE = "Hellooo World!";
 
     public App() {}
 
